2b933747e073c1501d5df601ac5466f1b839cf62
justin <justin.d.diaz@gmail.com>
2022-01-21 16:46:55 -0500

    test

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
+  const URL = process.env.REACT_APP_API_URL;
+  const [transactions, setTransactions] = useState([]);
+  let total = 0;
+
+  useEffect(() => {
+    axios
+      .get(`${URL}/transactions`)
+      .then((response) => setTransactions(response.data));
+  }, []);
+
   return (
     <nav>
       <h1>
@@ -13,6 +25,12 @@ export default function NavBar() {
           <button>New Transaction</button>
         </Link>
       </h1>
+      <div>
+        {transactions.map((transaction) => {
+          total += Number(transaction.amount);
+        })}
+        <h2>Total Amount: {total}</h2>
+      </div>
     </nav>
   );
 }
